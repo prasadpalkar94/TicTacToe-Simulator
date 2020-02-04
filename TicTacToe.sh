b878a51613e7f2a7679ba298b30dba1d1c666b60
prasadpalkar94 <explorehorizon15@gmail.com>
2020-02-04 18:50:16 +0530

    ADDED:Assigned letter Function for Play

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -3,6 +3,7 @@ declare -A board
 echo "-------WELCOME TO TIC TAC TOE-------"
 ROWS=3
 COLUMNS=3
+check=$((RANDOM%2))
 
 function displayBoard(){
 for ((i=1;i<=$ROWS;i++)){
@@ -11,4 +12,17 @@ board[$i,$j]=""
 }
 }
 }
+
+function assigned(){
+if [ $check -eq 1 ]
+then
+	player="X"
+	computer="O"
+else
+	computer="X"
+	player="O"
+fi
+}
+
 displayBoard
+assigned
