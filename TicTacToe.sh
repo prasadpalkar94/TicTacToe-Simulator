462d9769fdc1e117e34bd8aa9cf7d5866e59b46e
prasadpalkar94 <explorehorizon15@gmail.com>
2020-02-04 18:18:36 +0530

    ADDED:Reset Board Condition

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
+declare -A board
 echo "-------WELCOME TO TIC TAC TOE-------"
+ROWS=3
+COLUMNS=3
+
+function displayBoard(){
+for ((i=1;i<=$ROWS;i++)){
+for ((j=1;j<=$COLUMNS;j++)){
+board[$i,$j]=""
+}
+}
+}
+displayBoard
