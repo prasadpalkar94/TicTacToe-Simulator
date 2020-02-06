0a1093a6275c1c51a51c0ca69a608ff12be3ef04
prasadpalkar94 <explorehorizon15@gmail.com>
2020-02-06 16:41:40 +0530

    ADDED:checkEmptyCell Condition

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,29 +1,33 @@
-##!/bin/bash -x 
+#!/bin/bash
 declare -A board
 echo "-------WELCOME TO TIC TAC TOE-------"
+TOTALMOVE=9
 ROWS=3
 COLUMNS=3
 PLAYER=1
 COMPUTER=0
 check=$((RANDOM%2))
+movecount=0
 
 function resetBoard(){
-for ((i=1;i<=$ROWS;i++)){
-for ((j=1;j<=$COLUMNS;j++)){
-	board[$i,$j]=" "
-}
-}
+	for ((i=0;i<$ROWS;i++))
+	do
+		for ((j=0;j<$COLUMNS;j++))
+		do
+			board[$i,$j]="-"
+		done
+	done
 }
 
 function assigned(){
 if [ $check -eq $PLAYER ]
 then
 	player="X"
-	computer="O"
 else
-	computer="X"
 	player="O"
+
 fi
+echo player $player assigned
 }
 
 function toss(){
@@ -39,24 +43,31 @@ function displayBoard(){
 	for (( i=0; i<$ROWS; i++ ))
 	do
 			echo "-------------"
-			for (( j=0; j<$COLUMNS; j++ ))
+		for (( j=0; j<$COLUMNS; j++ ))
 		do
 			echo -n  "| ${board[$i,$j]} "
 		done
-		echo "|"
+			echo "|"
 	done
 				echo "-------------"
 }
 
-
-
-function checkWinORTiePlayer()
-{
-read -p "Enter the row for position" row
-read -p "Enter the column for position" column
+function checkEmptyCell(){
+	read -p "Enter row and column position" row column
+	if [[ ${board[$row,$column]} == "-" ]]
+	then
+		board[$row,$column]=$player
+		((movecount++))
+		displayBoard
+	else
+		echo "Position Occupied OR Invalid Position"
+	fi
+#checkEmptyCell
 }
+resetBoard
+assigned
+while [ $movecount -lt $TOTALMOVE ]
+do
+checkEmptyCell
+done
 
-displayBoard
-#resetBoard
-#assigned
-#toss
