04ad581f276fe10e10a204f7e45ca95572c32d23
prasadpalkar94 <prasadpalkar94@gmail.com>
2020-02-06 18:39:36 +0530

    ADDED:CheckEmptyCell Conditiongit add .

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,41 +1,72 @@
-#!/bin/bash -x
+#!/bin/bash 
 declare -A board
 echo "-------WELCOME TO TIC TAC TOE-------"
 ROWS=3
 COLUMNS=3
 PLAYER=1
-COMPUTER=0
+TOTALCOUNT=9
 check=$((RANDOM%2))
+movecount=1
+function resetBoard(){
+	for ((i=0;i<$ROWS;i++))
+	do
+		for ((j=0;j<$COLUMNS;j++))
+		do
+			board[$i,$j]="-"
+		done
+	done
+}
 
-function displayBoard(){
-for ((i=1;i<=$ROWS;i++)){
-for ((j=1;j<=$COLUMNS;j++)){
-board[$i,$j]=""
-}
-}
-}
-
-function assigned(){
-if [ $check -eq $PLAYER ]
-then
-	player="X"
-	computer="O"
-else
-	computer="X"
-	player="O"
-fi
+function assignSymbol(){
+	if [ $check -eq $PLAYER ]
+	then
+		player="X"
+	else
+		player="O"
+	fi
+	echo player $player assigned
 }
 
 function toss(){
-if [ $check -eq $PLAYER ]
-then
-echo PLAYER has won Toss
-else
-echo COMPUTER has won Toss
-fi
+	if [ $check -eq $PLAYER ]
+	then
+		echo PLAYER $player has won Toss
+	else
+		echo PLAYER $player has won Toss
+	fi
 }
 
+function displayBoard(){
+	echo "----------"
+	for (( i=0; i<$ROWS; i++ ))
+	do
+		for (( j=0; j<$COLUMNS; j++ ))
+		do
+			echo -n "| ${board[$i,$j]} "
+		done
+			echo "|"
+			echo "----------"
+	done
+}
 
-displayBoard
-assigned
-toss
+function checkEmptyCell(){
+	read -p "Enter row" row
+	read -p "Enter column" column
+	if [[ ${board[$row,$column]} == "-" ]]
+	then
+		board[$row,$column]=$player
+		((movecount++))
+		displayBoard
+	else
+		echo "Position Occupied Or Invalid Position"
+	fi
+}
+
+assignSymbol
+resetBoard
+while [[ $movecount -lt $TOTALCOUNT ]]
+do
+	checkEmptyCell
+done
+
+
